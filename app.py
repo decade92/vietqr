db017247b5ebdf93e46a40ca28b33a0548fe6b7e
decade92 <long.svnh4rum@gmail.com>
2025-06-16 13:33:29 +0700

    Update app.py

--- app.py
+++ app.py
@@ -163,7 +163,7 @@ def create_qr_with_background_thantai(data, acc_name, merchant_id):
     qr = qrcode.QRCode(error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border=0)
     qr.add_data(data)
     qr.make(fit=True)
-    qr_img = qr.make_image(fill_color="black", back_color="white").convert("RGBA").resize((520, 520))
+    qr_img = qr.make_image(fill_color="black", back_color="white").convert("RGBA").resize((500, 520))
     qr_img = round_corners(qr_img, radius=0)
     logo = Image.open(LOGO_PATH).convert("RGBA").resize((240, 80))
     qr_img.paste(logo, ((qr_img.width - logo.width) // 2, (qr_img.height - logo.height) // 2), mask=logo)
