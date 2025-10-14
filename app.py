4818b5d583c3e18309b58f61a2a14060146e5cc7
decade92 <long.svnh4rum@gmail.com>
2025-10-14 15:52:32 +0700

    Update app.py

--- app.py
+++ app.py
@@ -263,12 +263,23 @@ def create_qr_with_background_loa(data, acc_name, merchant_id, store_name="", su
     y_offset = qr_y + qr_img.height + 10
 
     # Tên tài khoản
+    label_acc = "Tên tài khoản:"
+    font_label = ImageFont.truetype(FONT_PATH, 28)
+    draw.text((qr_x + (qr_img.width - draw.textbbox((0,0), label_acc, font=font_label)[2]) // 2, y_offset), 
+              label_acc, fill="black", font=font_label)
+    
+    y_offset += font_label.size + 2
     font_acc = get_font(acc_name.upper(), max_text_width, 35)
     x_acc = qr_x + (qr_img.width - draw.textbbox((0,0), acc_name.upper(), font=font_acc)[2]) // 2
     draw.text((x_acc, y_offset), acc_name.upper(), fill=(0,102,102), font=font_acc)
-
-    # Merchant ID
-    y_offset += font_acc.size + 5
+    
+    # Số tài khoản
+    y_offset += font_acc.size + 10
+    label_merchant = "Số tài khoản:"
+    draw.text((qr_x + (qr_img.width - draw.textbbox((0,0), label_merchant, font=font_label)[2]) // 2, y_offset), 
+              label_merchant, fill="black", font=font_label)
+    
+    y_offset += font_label.size + 2
     font_merchant = get_font(merchant_id, max_text_width, 35)
     x_merchant = qr_x + (qr_img.width - draw.textbbox((0,0), merchant_id, font=font_merchant)[2]) // 2
     draw.text((x_merchant, y_offset), merchant_id, fill=(0,102,102), font=font_merchant)
