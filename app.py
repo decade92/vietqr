8af2566d24f4a245dbddf61b037429ea11500223
decade92 <long.svnh4rum@gmail.com>
2025-12-03 18:24:43 +0700

    Fix y_offset increment for support details
    Adjust y_offset increment for support name and phone.

--- app.py
+++ app.py
@@ -325,12 +325,12 @@ def create_qr_with_background(data, acc_name, merchant_id, store_name, support_n
     if support_name and support_name.strip():
         font_support_name = ImageFont.truetype(FONT_LABELPATH, 32)
         draw.text((x_label, y_offset), support_name, fill=(0,102,102), font=font_support_name)
-        y_offset += merchant_font_size + 60
+        y_offset += merchant_font_size + 1
     
     if support_phone and support_phone.strip():
         font_support_phone = ImageFont.truetype(FONT_LABELPATH, 32)
         draw.text((x_label, y_offset), support_phone, fill=(0,102,102), font=font_support_phone)
-        y_offset += merchant_font_size + 60
+        y_offset += merchant_font_size + 1
     # Store name
     store_font = ImageFont.truetype(FONT_PATH, 70)
     if store_name and store_name.strip():
