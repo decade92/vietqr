c8ea7c9ef213ce082ecffe72c572e1a4c5cdf46b
decade92 <long.svnh4rum@gmail.com>
2025-12-07 00:20:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -289,8 +289,8 @@ def create_qr_with_background(data, acc_name, merchant_id, store_name, support_n
     base.paste(qr_img, (qr_x, qr_y), qr_img)
 
     draw = ImageDraw.Draw(base)
-    font_branch = ImageFont.truetype(FONT_PATH, 50)  # cỡ font tùy chỉnh
-    draw.text((470, 80), "Chi nhánh", fill="#007C71", font=font_branch)
+    font_branch = ImageFont.truetype(FONT_PATH, 36)  # cỡ font tùy chỉnh
+    draw.text((465, 120), "Chi nhánh", fill="#3C7471", font=font_branch)
     # Font label
     font_label = ImageFont.truetype(FONT_LABELPATH, 46)
 
